7abc9e47535c39a44b469d0a3ef8f82cf75fdb9e
Mal Jones <mal@maljones.com>
2016-03-03 12:49:02 -0500

    Added check to Accept Invite form per requirements.

--- wp-content/themes/speak-agent/functions.php
+++ wp-content/themes/speak-agent/functions.php
@@ -154,7 +154,8 @@ function print_form_invite_req(){
 			<fieldset>
 				<div class="form-group">
 				<label for="i-email">Enter your email. <small class="label-required">required</small></label>
-				<input required type="email" id="i-email" name="i-email">
+				<input required type="email" id="i-email" name="i-email" maxlength="30">
+				<p class="i-msg" id="i-msg-email"><small><span id="i-chars-remaining">30</span> characters remaining (<strong>30</strong> character limit)</small></p>
 				</div>
 				<div class="form-group">
 				<label for="i-reason">Tell us why you\'d like an invitation. <small class="label-required">required</small></label>
@@ -211,7 +212,7 @@ function print_forgot_password_popup(){
 		<form>
 			<div class="form-group">
 			<label for="i-email">Enter your email. <small class="label-required">required</small></label>
-			<input required type="email" id="i-email" name="i-email">
+			<input required type="email" id="i-email" name="i-email" >
 			</div>
 			<div class="form-group">
 				<button class="is-full-width">Submit Request</button>
@@ -231,7 +232,8 @@ function print_accept_invite_popup(){
 			<p>Use your invite code to get engaging activities for your students!</p></div>
 			<div class="form-group">
 			<label for="r-email">Enter your email. <small class="label-required">required</small></label>
-			<input required type="email" id="r-email" name="r-email">
+			<input required type="email" id="r-email" name="r-email" maxlength="30">
+			<p class="i-msg" id="i-msg-email"><small><span id="i-chars-remaining">30</span> characters remaining (<strong>30</strong> character limit). This should be the same email you used to request the invite.</small></p>
 			</div>
 			<div class="form-group">
 			<label for="r-code">Enter your invite code. <small class="label-required">required</small></label>
